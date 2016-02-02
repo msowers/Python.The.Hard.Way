e9c2c158f87eddf4b2853d1f2d95e59ad079857d
msowers <msowers@gmail.com>
2016-02-02 13:00:16 -0700

    11, 12, 13

---
+++ ex13.1.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+script, age, height, weight = argv
+
+age = raw_input("How old are you? ")
+height = raw_input("What is your height? ")
+weight - raw_input("What is your weight? ")
+
+
+print "Your script is called:" , script
+print "Your first variable is:", age
+print "Your second variable is:", height
+print "Your third variable is:", weight
